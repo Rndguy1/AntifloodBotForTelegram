3e46d753b69fa13f733939a708f63d3a73955bef
Rndguy1 <spiderpork_m@libero.it>
2019-10-27 22:28:58 +0100

    Changes from fork

---
+++ antiflood.py
@@ -0,0 +1,43 @@
+import telepot
+from telepot.loop import MessageLoop
+from threading import Thread, Timer
+import time
+from datetime import datetime, timedelta
+from antiflood_config import token, antiflood_max_msgs, antiflood_seconds
+
+user_ids = [] #list stores messages user_id
+counter = {}  #dictionary of counters, organized by user id
+
+def antiflood(user_id, chat_id):
+    print('DEBUG - Values: user_id=' + str(user_id) + ' & chat_id=' + str(chat_id))
+    if counter.get(user_id) is not None and counter.get(user_id) >= antiflood_max_msgs:
+        date_time = datetime.now(None) + timedelta(seconds=60)
+        bot.restrictChatMember(chat_id, user_id, until_date=date_time.timestamp(), can_send_messages=False)
+        print("[!] " + str(user_id) + " e' stato bloccato per 60 secondi dopo aver inviato troppi messaggi")
+        
+    counter[user_id] = 0
+
+def on_message(msg):
+    # main function to be used
+    if msg['chat']['type'] == "supergroup" and "new_chat_member" not in msg and "left_chat_participant" not in msg:
+        user_id = str(msg['from']['id'])
+        if user_id not in user_ids:
+            user_ids.append(user_id)
+            counter[user_id] = 0
+        if counter[user_id] == 0:
+            Timer(antiflood_seconds, antiflood, [user_id, msg['chat']['id']]).start()
+            counter[user_id] = counter[user_id] + 1
+        else:
+            #tmp = counter.get(user_id) + 1
+            counter[user_id] = counter[user_id] + 1
+
+if __name__ == '__main__':
+    # script starting point
+    # defines telepot client
+    bot = telepot.Bot(token)
+    # loops the threaded main function
+    MessageLoop(bot, on_message).run_as_thread()
+
+    while True:
+        # used for receiving updates repeatedly
+        time.sleep(antiflood_seconds)
---
+++ antiflood_config.py
@@ -0,0 +1,5 @@
+token = "WRITE_YOUR_BOT_TOKEN_HERE"
+
+antiflood_seconds = 5
+antiflood_max_msgs = 4
+
